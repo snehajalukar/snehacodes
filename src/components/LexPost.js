2f4067ed3be222fd4ad6eedbcdfe1ef00dd583cb
Sneha Jalukar <shriyashjalukar@Snehas-Air.ht.home>
2023-10-15 11:51:35 -0500

    feat: styles header post box

--- src/components/LexPost.js
+++ src/components/LexPost.js
@@ -3,8 +3,22 @@ import React from 'react'
 const LexPost = ({title, description}) => {
   return (
     <div className="LexPost">
-        <div>{title}</div>
-        <div>{description}</div>
+        <div className="LexPost__header">
+          <div className="LexPost__header-left">
+            <div><b>Sneha</b> @sneha</div>
+            <div>Chicago, IL • Present</div>
+          </div>
+
+          <div className="LexPost__header-right">
+            <img width="25px" height="25px" src="horizontal-menu.png"></img>
+          </div>
+ 
+
+        </div>
+
+        <div>Content</div>
+
+        <div>Footer</div>
     </div>
   )
 }
