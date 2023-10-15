bd071698684ffe4f71225370764a65fe20a8e679
snehajalukar <sneha.jalukar@gmail.com>
2023-10-15 16:57:28 -0500

    feat: adds content and styles post boxes

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
     <div className="App">
       <Header />
       <div className="content">
-        <LexPost title="Box 1" description="This is the first white box." />
-        <LexPost title="Box 2" description="This is the second white box." />
-        <LexPost title="Box 3" description="This is the third white box." />
+        <LexPost time="Jan 2023 - Present" title="Software Engineer @ DebtBook" description="Currently, I work as a mid-level software engineer at DebtBook on the ecosystem team. I work within the Ruby/Rails and Javascript/React tech stacks!" />
+        <LexPost time="June 2021 - December 2022" title="Software Engineer @ Red Ventures" description="At Red Ventures, I worked as a full-stack software engineer on Bankrate, CreditCards.com and CNET Money. I used Typescript/React and PHP (Laravel) among other tech stacks." />
+        <LexPost time="Jan 2017 - May 2021" title="Computer Science Undergraduate Student" description="Graduated with my bachelors degree in Computer Science with a 3.8 GPA from Oakland University" />
       </div>
     </div>
   );
--- src/components/LexPost.js
+++ src/components/LexPost.js
@@ -1,26 +1,47 @@
 import React from 'react'
 
-const LexPost = ({title, description}) => {
+const LexPost = ({time, title, description}) => {
+
+  function sendEmail() {
+      window.location = "mailto:xyz@yourapplicationdomain.com";
+  }
+
   return (
     <div className="LexPost">
         <div className="LexPost__header">
           <div className="LexPost__header-left">
             <div><b>Sneha</b> @sneha</div>
-            <div>Chicago, IL • Present</div>
+            <div>Chicago, IL • {time}</div>
           </div>
 
           <div className="LexPost__header-right">
             <img width="25px" height="25px" src="horizontal-menu.png"></img>
           </div>
- 
-
         </div>
 
-        <div>Content</div>
 
-        <div>Footer</div>
+        <div className="LexPost__content">
+          <p className="LexPost__title">{title}</p>
+          <p className="LexPost__description">{description}</p>
+        </div>
+
+
+        <div className="LexPost__footer">
+          <div className="LexPost__footer-left">
+            <img width="22px" height="22px" src="heart.png"></img>
+          </div>
+          <div className="LexPost__footer-right">
+            <a href="mailto:sneha.jalukar@gmail.com">
+              <div className="LexPost__footer-box">
+              Reply
+              </div>
+            </a>
+          </div>
+        </div>
     </div>
   )
 }
+
+
 
 export default LexPost
