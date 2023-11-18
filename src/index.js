55df7ee9f317fba12a1e09d8046e1ee061fd5b74
snehajalukar <sneha.jalukar@gmail.com>
2023-11-18 12:09:26 -0500

    uses HashRouter for GitHub Pages support

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider,
 } from "react-router-dom";
+
+
 import Community from './Community';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
